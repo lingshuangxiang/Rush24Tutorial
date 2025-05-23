5e3cafc0b34de24254a4db745c3d043f5ca59613
danqiong.shen <danqiong.shen@unity.cn>
2025-05-23 16:55:47 +0800

    remove unused code

--- Assets/TwentyFour/Scripts/Gameplay/HomePage/StageButton.cs
+++ Assets/TwentyFour/Scripts/Gameplay/HomePage/StageButton.cs
@@ -14,18 +14,6 @@ namespace TwentyFour.Scripts.Gameplay.HomePage
         public TextMeshProUGUI textTMP;
 
         public int stageIndex;
-        
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         public void SetStageIndex(int index)
         {
