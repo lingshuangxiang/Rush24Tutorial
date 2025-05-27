0c3783217ea8174e25682a7eb41bb632d66e9d21
danqiong.shen <danqiong.shen@unity.cn>
2025-05-27 16:43:28 +0800

    remove editor scripts

--- Assets/TwentyFour/Editor/CustomPlayButton.cs
+++ Assets/TwentyFour/Editor/CustomPlayButton.cs
@@ -1,45 +1,48 @@
 ﻿#if UNITY_EDITOR
 using System.Reflection;
-using Unity.UOS.TwentyFour;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using TwentyFour.Scripts.Gameplay.HomePage;
 
-[InitializeOnLoad]
-public static class CustomPlayButton
+namespace TwentyFour.Scripts.Editors
 {
-    static CustomPlayButton()
+    [InitializeOnLoad]
+    public static class CustomPlayButton
     {
-        EditorApplication.update += OnUpdate;
-    }
-
-    private static ScriptableObject toolbar;
-    static void OnUpdate()
-    {
-        if (toolbar == null)
+        static CustomPlayButton()
         {
-            var toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
-            var toolbars = Resources.FindObjectsOfTypeAll(toolbarType);
-            toolbar = (ScriptableObject)toolbars[0];
-            var root = toolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
-            var visualRoot = root.GetValue(toolbar) as VisualElement;
-            var rightZone = visualRoot.Q("ToolbarZoneRightAlign");
+            EditorApplication.update += OnUpdate;
+        }
 
-            var container = new IMGUIContainer(() =>
+        private static ScriptableObject toolbar;
+
+        static void OnUpdate()
+        {
+            if (toolbar == null)
             {
-                GUILayout.BeginHorizontal();
-                if (GUILayout.Button("登出账号"))
+                var toolbarType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.Toolbar");
+                var toolbars = Resources.FindObjectsOfTypeAll(toolbarType);
+                toolbar = (ScriptableObject)toolbars[0];
+                var root = toolbar.GetType().GetField("m_Root", BindingFlags.NonPublic | BindingFlags.Instance);
+                var visualRoot = root.GetValue(toolbar) as VisualElement;
+                var rightZone = visualRoot.Q("ToolbarZoneRightAlign");
+
+                var container = new IMGUIContainer(() =>
                 {
-                    // EditorPrefs.SetBool("START_GAME_OPTION", true);
-                    // EditorApplication.ExecuteMenuItem("Edit/Play");
-                    var m = GameObject.FindObjectOfType<MainSceneManager>();
-                    m?.Logout();
-                }
-                GUILayout.EndHorizontal();
-            });
-            rightZone.Add(container);
+                    GUILayout.BeginHorizontal();
+                    if (GUILayout.Button("登出账号"))
+                    {
+                        // EditorPrefs.SetBool("START_GAME_OPTION", true);
+                        // EditorApplication.ExecuteMenuItem("Edit/Play");
+                        var m = GameObject.FindObjectOfType<MainSceneManager>();
+                        m?.Logout();
+                    }
+
+                    GUILayout.EndHorizontal();
+                });
+                rightZone.Add(container);
+            }
         }
     }
 }
--- Assets/TwentyFour/Editor/TutorialWindow.cs
+++ Assets/TwentyFour/Editor/TutorialWindow.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace Unity.UOS.TwentyFour.Editor
+namespace TwentyFour.Scripts.Editor
 {
     public class TutorialWindow : EditorWindow
     {
